549d65c8b451cb5dd16fcf73ea4d479b6057c061
Mitchell Harper <mitchwharper@gmail.com>
2014-10-18 05:20:44 -0700

    Add laptop creation form

--- app/controllers/laptops_controller.rb
+++ app/controllers/laptops_controller.rb
@@ -1,4 +1,17 @@
 class LaptopsController < ApplicationController
   def index
   end
+
+  def new
+    @laptop = Laptop.new
+  end
+
+  def create
+    @laptop = Laptop.new(params[:laptop])
+    if @laptop.save
+      redirect_to laptops_path
+    else
+      render action: "new"
+    end
+  end
 end
--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -3,4 +3,6 @@ class Laptop < ActiveRecord::Base
   belongs_to :classroom
   belongs_to :department 
   has_many :accessories
+
+  attr_accessible :person_id, :classroom_id, :department_id, :label_number
 end
