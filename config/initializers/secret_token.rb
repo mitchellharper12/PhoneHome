a9d790534cea41981ba797fc04de69fb43066b53
Mitchell Harper <mitchwharper@gmail.com>
2014-10-18 03:47:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhoneHome::Application.config.secret_token = 'b7054dfff834918a33232d79473d79298078aa87ed922077bbb1c081fd2df2ee97b9bb9719eb41d3da4a96d38012374bbe6c4ad0dd35de53d8ad74b7a441e964'
---
+++ readme.md
@@ -0,0 +1,4 @@
+# PhoneHome, a risk management app for AWW 2014
+## Mitchell Harper
+
+This application will be used by Admissions Workshop Weekend leadership for the University of Washington Dream Project to manage laptops checked out from university departments. Also included is the ability track laptops as they "Phone Home," or submit ajax requests to ensure connection to the UW network during the event and prevent loss of equipment.
