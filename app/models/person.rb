ecb48d9235fa3f5ff99a2d0245c7fdbb42fc0783
Mitchell Harper <mitchwharper@gmail.com>
2014-11-16 07:56:29 -0800

    Minor gameday updates

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ActiveRecord::Base
   belongs_to :classroom
   attr_accessible :classroom_id, :email, :firstname, :lastname, :netid, :phone, :type
+
+def fullname
+  firstname + ' ' + lastname
+end
 end
